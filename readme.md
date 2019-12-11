1fe761118f9027ef3159d06a27f22a9ec8b092ed
Raghav Misra <raghav.m2014@gmail.com>
2019-12-11 20:11:42 +0000

    bug fixes

---
+++ readme.md
@@ -0,0 +1,4 @@
+# CSSObject
+
+## What dis?
+CSSObject is a basic, yet intuitive CSS-in-JS implementation. 
--- script.js
+++ script.js
@@ -1,31 +1,86 @@
-const style = css(
-  "*", {
-    "padding": 0,
-    "margin": 0,
-    "box-sizing": "border-box",
-    "text-align": "center",
-    "font-family": "sans-serif"
+function ce(tag, attrs = {}, ...kids){
+  // Create Element
+  const el = document.createElement(tag);
+
+  // Add attributes
+  Object.keys(attrs).forEach((name) => {
+    if(attrs.hasOwnProperty(name)){
+      el.setAttribute(name, attrs[name]);
+    }
+  });
+
+  // Add children
+  kids.forEach((c) => {
+    if(typeof c == "string")
+      el.appendChild(document.createTextNode(c));
+    else el.appendChild(c);
+  });
+
+  // Return element
+  return el;
+}
+
+class StyledComponent extends HTMLElement {
+  constructor(){
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
   }
-).css(
-  "header", {
-    "background": "cornflowerblue",
-    "color": "white",
-    "padding": "10px"
+
+  connectedCallback(){
+    this.shadow.appendChild(this.style);
+    this.shadow.appendChild(this.render);
   }
-).css(
-  "body", {
-    "background": "royalblue",
-    "height": "100vh"
-  }  
-).css(
-  "p", {
-    "color": "royalblue",
-    "font-size": "20px"  
+}
+
+class test extends StyledComponent {
+  get style() {
+    return css(
+      "h1", {
+        "color": "royalblue",
+        "background": "cornflowerblue",
+        "padding": "10px",
+        "transition": "color 0.5s ease-in-out, background 0.5s ease-in-out",
+        "&:hover": {
+          "background": "#003366",
+          "color": "black"
+        }
+      }).css(
+      "h1::before", {
+        "content": "attr(text)"
+      }
+    ).toElement();
   }
-).css(
-  "*:not(html):not(body)", {
-    "margin": "5px;"
+
+  get render() {
+    return ce("h1", {}, "XDDD");
   }
+}
+
+customElements.define("test-el", test);
+
+
+const coolButton = {
+  "background": "black",
+  "color": "white",
+  "border": "2px blue solid",
+  "transition": "all 0.5s ease-in-out",
+
+  "&:hover": {
+    "background": "white",
+    "color": "black"
+  }
+};
+
+const btnStyle = (
+  css`button ${{
+  ...coolButton,
+  "&:before": {
+    "content": "attr(b4)"
+  },
+  "&:after": {
+    "content": "attr(aphtr)"
+  }
+  }}`
 ).toElement();
 
-document.body.appendChild(style);
+document.body.appendChild(btnStyle);
